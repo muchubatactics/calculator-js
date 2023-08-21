8453b5722cf475ad0b247ca77392d0b09f857903
muchubatactics <byooxmckeith@gmail.com>
2023-08-21 17:52:11 +0300

    has a few bugs but progress

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 /*
-The Burden of Being
-    ~muchubatactics 21/08/23
+    The Burden of Being
+        ~muchubatactics 21/08/23
 */
 
+//global variables
+
+let num1, num2, result;
+let operator;
+
+//functions
 
 function add(x, y)
 {
@@ -41,3 +47,101 @@ function operate(operator, x, y)
             console.log("error in operate");
     }
 }
+
+let display = document.querySelector(".display");
+
+let digitButtons = Array.from(document.querySelectorAll(".digit-buttons > button"));
+digitButtons.forEach( (btn) => {
+    btn.addEventListener("click", () => {
+        display.textContent += String(btn.textContent);
+    });
+});
+
+let addButton = document.querySelector(".addBtn");
+addButton.addEventListener("click", () => {
+    if (!operator)
+    {
+        operator = "+";
+        if(!num1)
+        {
+            num1 = Number(display.textContent);
+        }
+        else
+        {
+            num1 = result;
+        }
+        display.textContent = String(num1) + (" " + operator + " ");
+    }
+
+});
+
+let subtractButton = document.querySelector(".subBtn");
+subtractButton.addEventListener("click", () => {
+    if (!operator)
+    {
+        operator = "-";
+        if(!num1)
+        {
+            num1 = Number(display.textContent);
+        }
+        else
+        {
+            num1 = result;
+        }
+        display.textContent = String(num1) + (" " + operator + " ");
+    }
+});
+
+let multiplyButton = document.querySelector(".mulBtn");
+multiplyButton.addEventListener("click", () => {
+    if (!operator)
+    {
+        operator = "*";
+        if(!num1)
+        {
+            num1 = Number(display.textContent);
+        }
+        else
+        {
+            num1 = result;
+        }
+        display.textContent = String(num1) + (" " + operator + " ");
+    }
+});
+
+let divideButton = document.querySelector(".divBtn");
+divideButton.addEventListener("click", () => {
+    if (!operator)
+    {
+        operator = "/";
+        if(!num1)
+        {
+            num1 = Number(display.textContent);
+        }
+        else
+        {
+            num1 = result;
+        }
+        display.textContent = String(num1) + (" " + operator + " ");
+    }
+});
+
+let equalsButton = document.querySelector(".equBtn");
+equalsButton.addEventListener("click", () => {
+    num2 = Number(display.textContent.match(/\d+/g)[1]);
+    console.log(num1, operator, num2);
+    if (num2)
+    {
+        result = operate(operator, num1, num2);
+        display.textContent = String(result);
+    }
+});
+
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+    num1 = undefined;
+    num2 = undefined;
+    result = undefined;
+    operator = undefined;
+    display.textContent = '';
+});
