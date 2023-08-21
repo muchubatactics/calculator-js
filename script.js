ce1c09b4d7f484d5cec4dff0188eafe29a5399bd
muchubatactics <byooxmckeith@gmail.com>
2023-08-21 12:25:34 +0300

    progress

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+The Burden of Being
+    ~muchubatactics 21/08/23
+*/
+
+
+function add(x, y)
+{
+    return x + y;
+}
+
+function subtract(x, y)
+{
+    return x - y;
+}
+
+function multiply(x, y)
+{
+    return x * y;
+}
+
+function divide(x, y)
+{
+    return x / y;
+}
+
+function operate(operator, x, y)
+{
+    switch (operator)
+    {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+        //debug
+        default:
+            console.log("error in operate");
+    }
+}
